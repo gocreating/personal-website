4d3507c085945d8d219b8ff871e4fe18c2fe9672
翁治平 <gocreating@gmail.com>
2016-10-01 05:05:12 +0800

    Improve ListPage UI

--- src/common/components/pages/blog/post/ListPage.js
+++ src/common/components/pages/blog/post/ListPage.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import cx from 'classnames';
 import { setPosts } from '../../../../actions/blogActions';
 import PageLayout from '../../../layouts/PageLayout';
+import Section from '../../../Section';
 import Container from '../../../main/Container';
+import Time from '../../../Time';
 import blogAPI from '../../../../api/blog';
 
 class ListPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      column: 1,
+    };
+  }
+
   componentDidMount() {
     let { blog: { isPostsFetched }, apiEngine, dispatch } = this.props;
 
@@ -22,30 +32,91 @@ class ListPage extends Component {
     }
   }
 
+  abstractToComponents(abstract) {
+    let lines = abstract.split('\n');
+    let lastIndex = lines.length - 1;
+    return lines.map((line, index) => (
+      index === lastIndex ? line : [line, <br />]
+    ));
+  }
+
+  renderToolbar() {
+    let { column } = this.state;
+    let cxBtnOfColumn = (c) => (
+      cx('btn btn-default', {active: c === column})
+    );
+
+    return (
+      <div className="visible-md visible-lg btn-group pull-right">
+        <button
+          type="button"
+          className={cxBtnOfColumn(1)}
+          onClick={() => this.setState({ column: 1 })}
+        >
+          <i className="fa fa-th-list" aria-hidden="true" />
+        </button>
+        <button
+          type="button"
+          className={cxBtnOfColumn(2)}
+          onClick={() => this.setState({ column: 2 })}
+        >
+          <i className="fa fa-th-large" aria-hidden="true" />
+        </button>
+        <button
+          type="button"
+          className={cxBtnOfColumn(3)}
+          onClick={() => this.setState({ column: 3 })}
+        >
+          <i className="fa fa-th" aria-hidden="true" />
+        </button>
+      </div>
+    );
+  }
+
   render() {
     let { blog: { posts } } = this.props;
+    let { column } = this.state;
 
     return (
       <PageLayout>
-        <section>
+        <Section
+          style={{
+            backgroundColor: '#eee',
+          }}
+        >
+          <Container>
+            {this.renderToolbar()}
+          </Container>
           <Container>
             <div className="row">
-              <div className="col-lg-12">
-                {posts.length > 0 ? (
-                  <ul>
-                    {posts.map((post) => (
-                      <li key={post.slug}>
-                        <Link to={`/blog/post/${post.slug}`}>{post.title}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                ) : (
-                  <p>尚無文章</p>
-                )}
-              </div>
+              {posts.map((post) => (
+                <div
+                  key={post._id}
+                  className={`col-md-${12 / column}`}
+                  style={{margin: '20px 0'}}
+                >
+                  <div className="thumbnail">
+                    <div className="caption" style={{padding: 60}}>
+                      <h1 style={{marginBottom: 50}}>{post.title}</h1>
+                      <p>
+                        {this.abstractToComponents(post.abstract)}...
+                        <Link to={`/blog/post/${post.slug}`}>繼續閱讀</Link>
+                      </p>
+                      <hr />
+                      <span style={{color: '#aaa'}}>
+                        <Time
+                          format="YYYY-MM-DD"
+                          value={post.createdAt}
+                          relative
+                        />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </Container>
-        </section>
+        </Section>
       </PageLayout>
     );
   }
